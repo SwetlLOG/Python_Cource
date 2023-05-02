09b40922df6a6f497879248e2005eebb01dab44c
SwetlLOG <sweetlog@mail.ru>
2023-05-02 09:04:46 +0400

    Задача 1 к семинару 6

---
+++ DZ_Sem6_Task_1.py
@@ -0,0 +1,5 @@
+first_el = int(input('Введите первый элемент  '))
+dif_el = int(input('Введите разность элементов  '))
+n = int(input('введите количество элементов  '))
+for i in range(n):
+    print(first_el + i * dif_el)
